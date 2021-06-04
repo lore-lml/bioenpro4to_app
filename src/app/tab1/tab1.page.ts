bde6f734ca615fa8f0575216be7cacf7905b98e1
Lorenzo Limoli <lorenzo.limoli@outlook.it>
2021-06-04 19:50:50 +0200

    monitor view

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -7,6 +7,24 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
+  title = 'Monitora';
+  categoryOpts = {
+    freeMode: true,
+    slidesPerView: 2.5
+  };
+  categoryInfo = [
+    {title: 'Camion',imgSrc: 'assets/categories/trucks.svg'},
+    {title: 'Pesate', imgSrc: 'assets/categories/scales.svg'},
+    {title: 'Biocelle', imgSrc: 'assets/categories/biocells.svg'},
+  ];
+  lastUpdate = [
+    {imgSrc: 'assets/categories/trucks.svg', title: 'Actor1', timestamp: '10.55 - 21/05/2012'},
+    {imgSrc: 'assets/categories/trucks.svg', title: 'Actor2', timestamp: '9.55 - 20/05/2012'},
+    {imgSrc: 'assets/categories/scales.svg', title: 'Actor3', timestamp: '8.55 - 19/05/2012'},
+    {imgSrc: 'assets/categories/biocells.svg', title: 'Actor4', timestamp: '7.55 - 18/05/2012'},
+    {imgSrc: 'assets/categories/biocells.svg', title: 'Actor5', timestamp: '6.55 - 17/05/2012'},
+    {imgSrc: 'assets/categories/scales.svg', title: 'Actor6', timestamp: '5.55 - 16/05/2012'},
+  ];
   constructor() {}
 
 }
--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -7,6 +7,22 @@ import { Component } from '@angular/core';
 })
 export class TabsPage {
 
-  constructor() {}
+  tabs: any;
+  selectedTab: number;
+  constructor() {
+    this.tabs = [
+      {title: 'Monitora', icon: 'compass'},
+      {title: 'Statistiche', icon: 'stats-chart'},
+      {title: 'Profilo', icon: 'person'}
+    ];
+  }
 
+  setSelected(tab: string) {
+    const t = tab.substr(3);
+    this.selectedTab = parseInt(t, 10);
+  }
+
+  getIcon(i: number) {
+    return i+1 === this.selectedTab ? this.tabs[i].icon : `${this.tabs[i].icon}-outline`;
+  }
 }
